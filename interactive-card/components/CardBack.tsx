df49224d6f7c78a74fc914bb2a3fc4cfaf911d27
HzamiBaha <bahaeddine.hzami@gmail.com>
2022-09-12 15:03:49 +0100

    responsive design part

---
+++ interactive-card/components/CardBack.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../styles/CardFront.module.css'
+
+export default function CardBack() {
+  return (
+    <div className={styles.cardWrapperback}></div>
+  )
+}
---
+++ interactive-card/components/CardFront.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../styles/CardFront.module.css'
+
+export default function CardFront() {
+  return (
+    <div className={styles.cardWrapper}></div>
+  )
+}
